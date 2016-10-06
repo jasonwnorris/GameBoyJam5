81dec6a90bb59a71302efc66dd9df8b76ddb5dcc
jasonwnorris <jasonwellsnorris@gmail.com>
2016-10-05 23:53:17 -0500

    Basic player jumping.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -10,12 +10,18 @@ public class PlayerController : MonoBehaviour
     const string c_AnimationRunRight = "anim_player_run_right";
 
     const string c_AxisHorizontal = "Horizontal";
+    const string c_ButtonJump = "Jump";
+
+    const float c_RaycastDistance = 12.125f;
 
     #endregion
 
     #region Public Variables
 
     public float HorizontalMovementSpeed = 1.0f;
+    public float JumpingSpeed = 2.5f;
+    public float MinimumJumpTime = 0.15f;
+    public float MaximumJumpTime = 0.5f;
 
     #endregion
 
@@ -26,6 +32,9 @@ public class PlayerController : MonoBehaviour
     private Animator m_Animator;
 
     private bool m_IsFacingRight;
+    private bool m_IsOnGround;
+    private bool m_IsJumping;
+    private float m_JumpTime;
 
     #endregion
 
@@ -38,16 +47,57 @@ public class PlayerController : MonoBehaviour
         m_Animator = GetComponent<Animator>();
 
         m_IsFacingRight = true;
+        m_IsOnGround = false;
+        m_IsJumping = false;
+        m_JumpTime = 0.0f;
     }
 
     void Update()
     {
+        // Raycast to check if on ground.
+        RaycastHit2D raycastHit = Physics2D.Raycast(m_Transform.position, Vector2.down, c_RaycastDistance);
+        if (raycastHit)
+        {
+            m_IsOnGround = true;
+        }
+        else
+        {
+            m_IsOnGround = false;
+        }
+
+        // Read input.
         float horizontal = Input.GetAxis(c_AxisHorizontal);
+        bool isJumpButtonDown = Input.GetButton(c_ButtonJump);
 
-        Vector2 force = Vector2.right * horizontal * HorizontalMovementSpeed;
+        // Handle movement.
+        Vector2 horizontalForce = Vector2.right * horizontal * HorizontalMovementSpeed;
+        m_Rigidbody.AddRelativeForce(horizontalForce, ForceMode2D.Impulse);
 
-        m_Rigidbody.AddRelativeForce(force, ForceMode2D.Impulse);
+        // Handle jump input.
+        if (isJumpButtonDown && m_IsOnGround)
+        {
+            if (!m_IsJumping)
+            {
+                m_IsJumping = true;
+                m_JumpTime = 0.0f;
+            }
+        }
 
+        // Increase jumping over time.
+        if (m_IsJumping)
+        {
+            Vector2 verticalForce = Vector2.up * JumpingSpeed;
+            m_Rigidbody.AddRelativeForce(verticalForce, ForceMode2D.Impulse);
+
+            m_JumpTime += Time.deltaTime;
+
+            if (m_JumpTime >= MaximumJumpTime || (!isJumpButtonDown && m_JumpTime > MinimumJumpTime))
+            {
+                m_IsJumping = false;
+            }
+        }
+
+        // Change states based on input.
         if (horizontal > 0.0f)
         {
             m_Animator.Play(c_AnimationRunRight);
