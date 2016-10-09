53b3223f98aa045c2995641c45ce7a64467485b3
jasonwnorris <jasonwellsnorris@gmail.com>
2016-10-09 08:18:16 -0500

    FixedUpdate -> Update (again)

--- Assets/Scripts/LockBoundary.cs
+++ Assets/Scripts/LockBoundary.cs
@@ -1,38 +1,60 @@
 ﻿using System;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class LockBoundary : MonoBehaviour
 {
-    private class Lock
+    public enum Direction
     {
-        public Vector2 RayDirection { get; set; }
-        public Vector3 AdjustmentDirection { get; set; }
-        public Func<float> DistanceFunction { get; set; }
+        Up,
+        Down,
+        Left,
+        Right
     }
+
+    [Serializable]
+    public class ScanParameter
+    {
+        public Direction Direction;
+
+        [Range(0.0f, 1024.0f)]
+        public float Distance;
+    }
+
+    #region Constants
+
+    private static readonly Dictionary<Direction, Vector2> c_RayDirections = new Dictionary<Direction, Vector2>()
+    {
+        { Direction.Up, Vector2.up },
+        { Direction.Down, Vector2.down },
+        { Direction.Left, Vector2.left },
+        { Direction.Right, Vector2.right }
+    };
+
+    private static readonly Dictionary<Direction, Vector3> c_OffsetDirections = new Dictionary<Direction, Vector3>()
+    {
+        { Direction.Up, Vector3.down },
+        { Direction.Down, Vector3.up },
+        { Direction.Left, Vector3.right },
+        { Direction.Right, Vector3.left }
+    };
+
+    #endregion
 
     #region Public Variables
 
     public LayerMask LayerMask;
 
     [Range(0.0f, 1024.0f)]
-    public float TopDistance;
+    public float BoxSize;
 
-    [Range(0.0f, 1024.0f)]
-    public float BottomDistance;
-
-    [Range(0.0f, 1024.0f)]
-    public float LeftDistance;
-
-    [Range(0.0f, 1024.0f)]
-    public float RightDistance;
+    public ScanParameter[] ScanParameters;
 
     #endregion
 
     #region Members
 
     private Transform m_Transform;
-
-    private Lock[] m_Locks;
 
     #endregion
 
@@ -41,49 +63,30 @@ public class LockBoundary : MonoBehaviour
     void Start()
     {
         m_Transform = GetComponent<Transform>();
-
-        m_Locks = new Lock[]
-        {
-            new Lock()
-            {
-                RayDirection = Vector2.up,
-                AdjustmentDirection = Vector3.down,
-                DistanceFunction = () => { return TopDistance; }
-            },
-            new Lock()
-            {
-                RayDirection = Vector2.down,
-                AdjustmentDirection = Vector3.up,
-                DistanceFunction = () => { return BottomDistance; }
-            },
-            new Lock()
-            {
-                RayDirection = Vector2.left,
-                AdjustmentDirection = Vector3.right,
-                DistanceFunction = () => { return LeftDistance; }
-            },
-            new Lock()
-            {
-                RayDirection = Vector2.right,
-                AdjustmentDirection = Vector3.left,
-                DistanceFunction = () => { return RightDistance; }
-            }
-        };
     }
 
     void Update()
     {
-        Vector2 position = Tools.ToVector2(m_Transform.position);
-
-        foreach (Lock l in m_Locks)
+        Vector2 size = new Vector2(BoxSize, BoxSize);
+        float halfSize = BoxSize * 0.5f;
+        foreach (ScanParameter sp in ScanParameters)
         {
-            float distance = l.DistanceFunction();
-            RaycastHit2D raycastHit = Physics2D.Raycast(position, l.RayDirection, distance, LayerMask);
+            Vector2 position = Tools.ToVector2(m_Transform.position);
+            RaycastHit2D raycastHit = Physics2D.BoxCast(position, size, 0.0f, c_RayDirections[sp.Direction], sp.Distance, LayerMask);
             if (raycastHit.collider != null)
             {
-                m_Transform.Translate(l.AdjustmentDirection * (distance - raycastHit.distance));
+                Vector2 d = c_RayDirections[sp.Direction];
+                Vector2 n = raycastHit.normal;
+                Debug.Log(string.Format("[{0:0.00}] Direction: {1}, Normal: {2}, Dot: {3}, Distance: {4:0.000}", Time.time, d, n, Vector2.Dot(d, n), raycastHit.distance));
+                m_Transform.Translate(c_OffsetDirections[sp.Direction] * (sp.Distance - raycastHit.distance - halfSize));
             }
         }
+    }
+
+    void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireCube(transform.position, Vector3.one * BoxSize);
     }
 
     #endregion
