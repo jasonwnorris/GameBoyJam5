abc5dad404c452bd0e5506ef803b0fc0f1bf71b7
jasonwnorris <jasonwellsnorris@gmail.com>
2016-10-09 04:57:17 -0500

    Implemented health/damage system.

---
+++ Assets/Scripts/Damage.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class Damage : MonoBehaviour
+{
+    #region Public Variables
+
+    [Range(0, 100)]
+    public int Health;
+
+    [Range(0.0f, float.MaxValue)]
+    public float Knockback;
+
+    #endregion
+}
